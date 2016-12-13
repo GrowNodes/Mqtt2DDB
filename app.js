ac7ea8f53086c3d3342f77c583d67a2de82489e3
Kevin Yeh <keyeh@calpoly.edu>
2016-12-12 21:54:26 -0800

    demo push notifications

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const request = require('request');
 const uuidV4 = require('uuid/v4');
 var mqtt=require('mqtt')  
 
@@ -15,7 +16,7 @@ mqttClient.subscribe(deviceRoot+"#")
 mqttClient.on('message', insertEvent);
 
 function insertEvent(topic,payload) {  
-  // console.log("new msg", topic, payload.toString())
+  const msg_text = payload.toString()
   const topic_without_root = topic.replace(deviceRoot,'')
   // var node_serial = topic_without_root.split('/')[0];
   // var sub_topic = topic_without_root.substring(topic_without_root.indexOf("/") + 1);
@@ -24,9 +25,44 @@ function insertEvent(topic,payload) {
     message_id: uuidV4(),
     timestamp: Date.now(),
     topic: topic_without_root,
-    body: payload.toString()
+    body: msg_text
 
   }
   ddb.putItem('mqtt_log', item, {}, function(err, res, cap) {});
 
+
+  if (topic_without_root == "5ccf7f94ee4f/$online") {
+    const title = `5ccf7f94ee4f is now ${msg_text == "true" ? "online" : "offline"}`
+    sendMessage(title, "Tap for details")
+  }
+}
+
+function sendMessage(title, body, callback) {
+  var options = {
+    url: 'https://fcm.googleapis.com/fcm/send',
+    method: 'POST',
+    headers: {
+      'Authorization': 'key='+process.env.FCM_KEY
+    },
+    json: {
+  "notification":{
+    title,
+    body,
+    "sound":"default", 
+    "click_action":"FCM_PLUGIN_ACTIVITY",  
+    "icon":"fcm_push_icon"  
+  },
+    "to":"/topics/all", 
+    "priority":"high" 
+}
+  }
+
+  request(options, function (error, response, body) {
+    console.log(body)
+    if (!error && response.statusCode == 200) {
+        // callback(body)
+    } else {
+      // callback(error)
+    }
+  })
 }
