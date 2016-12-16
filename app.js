11971d6e6b9394124b54f4255cc8b22630a9b702
Kevin Yeh <keyeh@calpoly.edu>
2016-12-16 03:20:51 -0800

    preliminary notification system

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const request = require('request');
 const uuidV4 = require('uuid/v4');
-var mqtt=require('mqtt')  
+const mqtt = require('mqtt')  
+const mqtt_regex = require("mqtt-regex");
 
-var ddb = require('dynamodb').ddb({
+
+const ddb = require('dynamodb').ddb({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   endpoint: 'dynamodb.us-east-1.amazonaws.com'
 });
 
+const deviceRoot="nodes/"
 
-var deviceRoot="nodes/"  
+const mqttClient = mqtt.connect('mqtt://demo.grownodes.com')
 
-var mqttClient=mqtt.connect('mqtt://demo.grownodes.com')
+
+
 mqttClient.subscribe(deviceRoot+"#")
-mqttClient.on('message', insertEvent);
+mqttClient.on('message', handleMsg);
 
-function insertEvent(topic,payload) {  
+
+
+function handleMsg(topic, payload) {
+  saveMsg(topic, payload)
+  sendToFcmHandlerIfNeeded(topic, payload)
+}
+
+function saveMsg(topic, payload) {  
   const msg_text = payload.toString()
   const topic_without_root = topic.replace(deviceRoot,'')
   // var node_serial = topic_without_root.split('/')[0];
@@ -29,36 +40,36 @@ function insertEvent(topic,payload) {
 
   }
   ddb.putItem('mqtt_log', item, {}, function(err, res, cap) {});
-
-
-  if (topic_without_root == "5ccf7f94ee4f/$online") {
-    const title = `5ccf7f94ee4f is now ${msg_text == "true" ? "online" : "offline"}`
-    sendMessage(title, "Tap for details")
-  }
 }
 
-function sendMessage(title, body, callback) {
+
+function sendToFcmHandlerIfNeeded(topic, payload) {
+  const topicParser = mqtt_regex(deviceRoot+"+serial/notification/send").exec;
+  const parsedTopic = topicParser(topic);
+
+  if (!parsedTopic) {
+    return
+  }
+
+  console.log("sending notification: ", payload.toString())
+try {
+  var msgObj = {body: payload.toString()}
+} catch(e) {
+  console.error("Error parsing JSON:", payload.toString())
+  return
+}
+  msgObj.serial = parsedTopic.serial
+
   var options = {
-    url: 'https://fcm.googleapis.com/fcm/send',
+    url: process.env.NOTIFIER_SEND_URL,
     method: 'POST',
     headers: {
       'Authorization': 'key='+process.env.FCM_KEY
     },
-    json: {
-  "notification":{
-    title,
-    body,
-    "sound":"default", 
-    "click_action":"FCM_PLUGIN_ACTIVITY",  
-    "icon":"fcm_push_icon"  
-  },
-    "to":"/topics/all", 
-    "priority":"high" 
-}
+    json: msgObj
   }
 
   request(options, function (error, response, body) {
-    console.log(body)
     if (!error && response.statusCode == 200) {
         // callback(body)
     } else {
